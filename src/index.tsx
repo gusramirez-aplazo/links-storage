a423b81243514a1d6f11bce99841fb9feeceb257
Gus Ramírez <gustavo@aplazo.mx>
2024-10-11 12:37:49 -0600

    feat: add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { sentry } from '@hono/sentry'
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { secureHeaders } from 'hono/secure-headers'
@@ -8,6 +9,7 @@ const app = new Hono()
 
 app.use(secureHeaders())
 app.use(logger())
+app.use('*', sentry())
 
 app.route('/api', api)
 
